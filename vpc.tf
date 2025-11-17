31a50b620aac03c56018bf933bf151c003ce742d
Rajkumar <rajkumarmanda@outlook.com>
2025-11-17 02:18:23 -0500

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,22 +1,23 @@
+# VPC
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
-  tags = merge (
+  tags = merge(
     var.vpc_tags,
     local.common_tags,
     {
         Name = local.common_name_suffix
     }
   )
-
 }
 
+# IGW
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge (
+  tags = merge(
     var.igw_tags,
     local.common_tags,
     {
@@ -25,6 +26,7 @@ resource "aws_internet_gateway" "main" {
   )
 }
 
+# Public Subnets
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
@@ -32,49 +34,54 @@ resource "aws_subnet" "public" {
   availability_zone = local.az_names[count.index]
   map_public_ip_on_launch = true
 
-  tags = merge (
+  tags = merge(
     var.public_subnet_tags,
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-public-${local.az_names[count.index]}"  ### roboshop-dev-public-us-east-1a
+        Name = "${local.common_name_suffix}-public-${local.az_names[count.index]}" # roboshop-dev-public-us-east-1a
     }
   )
 }
 
+
+# Private Subnets
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
 
-  tags = merge (
+  tags = merge(
     var.private_subnet_tags,
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-private-${local.az_names[count.index]}"  ### roboshop-dev-private-us-east-1a
+        Name = "${local.common_name_suffix}-private-${local.az_names[count.index]}" # roboshop-dev-private-us-east-1a
     }
   )
 }
 
+# Database Subnets
 resource "aws_subnet" "database" {
   count = length(var.database_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.database_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
 
-  tags = merge (
+  tags = merge(
     var.database_subnet_tags,
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-database-${local.az_names[count.index]}"  ### roboshop-dev-database-us-east-1a
+        Name = "${local.common_name_suffix}-database-${local.az_names[count.index]}" # roboshop-dev-database-us-east-1a
     }
   )
 }
 
+
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge (
+  tags = merge(
     var.public_route_table_tags,
     local.common_tags,
     {
@@ -83,10 +90,12 @@ resource "aws_route_table" "public" {
   )
 }
 
+
+# Private Route Table
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge (
+  tags = merge(
     var.private_route_table_tags,
     local.common_tags,
     {
@@ -95,10 +104,12 @@ resource "aws_route_table" "private" {
   )
 }
 
+
+# Database Route Table
 resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge (
+  tags = merge(
     var.database_route_table_tags,
     local.common_tags,
     {
@@ -107,16 +118,18 @@ resource "aws_route_table" "database" {
   )
 }
 
+# Public Route
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.main.id
 }
 
+# Elastic IP
 resource "aws_eip" "nat" {
   domain   = "vpc"
 
-  tags = merge (
+  tags = merge(
     var.eip_tags,
     local.common_tags,
     {
@@ -125,15 +138,17 @@ resource "aws_eip" "nat" {
   )
 }
 
+
+# NAT gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
 
-  tags = merge (
+  tags = merge(
     var.nat_gateway_tags,
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-nat"
+        Name = "${local.common_name_suffix}"
     }
   )
 
@@ -142,12 +157,14 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [aws_internet_gateway.main]
 }
 
+# Private egress route through NAT
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.nat.id
 }
 
+# Database egress route through NAT
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
