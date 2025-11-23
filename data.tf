89d22bcad46a478ed4d2ed590b4aca62646dcd73
Rajkumar <rajkumarmanda@outlook.com>
2025-11-23 01:38:18 -0500

    terraform

--- data.tf
+++ data.tf
@@ -8,7 +8,6 @@ data "aws_vpc" "default" {
 
 data "aws_route_table" "main" {
   vpc_id = data.aws_vpc.default.id
-
   filter {
     name   = "association.main"
     values = ["true"]
--- variable.tf
+++ variable.tf
@@ -1,75 +1,80 @@
 variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+  type        = string
+  description = "Please provide VPC CIDR"
 }
 
 variable "project_name" {
-    default = "roboshop"
+  type = string
 }
 
 variable "environment" {
-    default = "dev"
+  type = string
 }
 
 variable "vpc_tags" {
+    type = map
     default = {}
 }
 
 variable "igw_tags" {
+    type = map
     default = {}
 }
 
 variable "public_subnet_cidrs" {
     type = list
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnet_tags" {
+    type = map
     default = {}
 }
 
+
 variable "private_subnet_cidrs" {
     type = list
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "private_subnet_tags" {
+    type = map
     default = {}
 }
 
 variable "database_subnet_cidrs" {
     type = list
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
 variable "database_subnet_tags" {
+    type = map
     default = {}
 }
 
 variable "public_route_table_tags" {
+    type = map
     default = {}
 }
 
 variable "private_route_table_tags" {
+    type = map
     default = {}
 }
 
 variable "database_route_table_tags" {
+    type = map
     default = {}
 }
 
 variable "eip_tags" {
+    type = map
     default = {}
 }
 
 variable "nat_gateway_tags" {
+    type = map
     default = {}
 }
 
 variable "is_peering_required" {
     type = bool
     default = true
-}
-
-variable "peering_tags" {
-    default = {}
 }
